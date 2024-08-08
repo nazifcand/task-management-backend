e1eb1057bfaa142f45c2f3559aead8a2c9324d3f
Nazif Can DURGUT <nazifcandurgut@icloud.com>
2024-08-08 23:05:56 +0300

    clear app service & controller

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/models/User.model';
 
 @Injectable()
-export class AppService {
-  async getAllUsers(): Promise<User[]> {
-    return await User.findAll({
-      attributes: { exclude: ['createdAt', 'updatedAt', 'password'] },
-    });
-  }
-}
+export class AppService {}
